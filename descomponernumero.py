9acc1ccfd0e2079d8271f92ebc19a67e5cb38825
juliocmezar <juliomeza2512@gmail.com>
2024-08-19 12:51:28 -0500

    Update descomponernumero.py
    tres

--- descomponernumero.py
+++ descomponernumero.py
@@ -1,6 +1,5 @@
-print("hola")
-print("asdasda adasd aasdasd ")
-print("hola")
-print("hola")
-print("hola")
-print("hola")
+for i in range(1,300):
+ if i%2==0:
+  print(i, "es par")
+ else:
+  print(i, "es impar")
